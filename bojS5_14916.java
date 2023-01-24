6ebac3c419b892b98f24fdbcb1b796b56c247cbf
devirus <devrusgg@gmail.com>
2023-01-24 18:40:24 +0900

    java: bojS5_14916_거스름돈

---
+++ bojS5_14916.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bojS5_14916 {  // 거스름돈
+
+    public static void main(String[] args) throws IOException {
+        new bojS5_14916().solution();
+    }
+
+    private void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while (n > 0) {
+            if (n % 5 == 0) {
+                cnt += n / 5;
+                n %= 5;
+            } else {
+                n -= 2;
+                cnt++;
+            }
+        }
+        if (n != 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(cnt);
+        }
+    }
+
+}
