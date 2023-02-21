a08a0fffb012f540dcdf859657e30f21ff66a622
devirus <devrusgg@gmail.com>
2023-02-21 18:33:08 +0900

    java: BojS5_11651_좌표 정렬하기 2 lambda 버전 풀이

--- BojS5_11651.java
+++ BojS5_11651.java
@@ -8,7 +8,32 @@ import java.util.StringTokenizer;
 public class BojS5_11651 {  // 좌표 정렬하기 2
 
     public static void main(String[] args) throws IOException {
-        new BojS5_11651().solution();
+        new BojS5_11651().solution_lambda();
+    }
+
+    private void solution_lambda() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            } else {
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
     }
 
     private void solution() throws IOException {
