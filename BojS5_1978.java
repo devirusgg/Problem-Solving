06c71b1fd2d24e7bccbf6dd5e24519cd6debb545
devirus <devrusgg@gmail.com>
2023-02-24 18:37:51 +0900

    java: BojS5_1978_소수 찾기 이전 코드는 잘못 커밋됨

--- BojS5_1978.java
+++ BojS5_1978.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BojS5_1978 {   // 소수 찾기
 
@@ -10,18 +11,33 @@ public class BojS5_1978 {   // 소수 찾기
 
     private void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        int num = 665;
+        int n = Integer.parseInt(br.readLine());
         int cnt = 0;
 
-        while (cnt != N) {
-            num++;
-            if (String.valueOf(num).contains("666")) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            boolean isPrime = true;
+
+            if (num <= 3) {
+                if (num > 1) {
+                    cnt++;
+                }
+                continue;
+            }
+
+//            for (int i = 2; i < num; i++) {   // 전부 탐색
+            for (int i = 2; i <= Math.sqrt(num); i++) {   // 제곱근 이용하면 탐색을 더 줄일 수 있음
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
                 cnt++;
             }
         }
-        System.out.println(num);
+        System.out.println(cnt);
     }
 
 }
