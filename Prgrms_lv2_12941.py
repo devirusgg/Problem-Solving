2b8765c40623a5e275dd19b710a497f2fc0be393
devirus <devrusgg@gmail.com>
2023-03-11 18:19:07 +0900

    python: Prgrms_lv2_12941_최솟값 만들기

---
+++ Prgrms_lv2_12941.py
@@ -0,0 +1,13 @@
+# 최솟값 만들기
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort(reverse=True)
+    for i in range(len(A)):
+        answer += (A[i] * B[i])
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([1, 4, 2], [5, 4, 4]))
+    print(solution([1, 2], [3, 4]))
